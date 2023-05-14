d01944bee9f86026a33d8e93f828eb55e8f70c5d
SIBUSISOMT <sbusisozamowakhe18@gmail.com>
2023-05-15 01:27:26 +0200

    functionality complete

--- main.js
+++ main.js
@@ -3,13 +3,11 @@
 const name = document.getElementById("name");
 const totalAmount = document.getElementById("total-amount");
 const submit = document.getElementById("submit");
-const totalAmountButton = document.getElementById("total-amount-button");
 let incomeAmount = document.getElementById("Income-amount");
 let expenditureValue = document.getElementById("expenditure-value");
 let balanceAmount = document.getElementById("balance-amount");
 const listContainer = document.getElementById("list");
 const productTitle = document.getElementById("product-title");
-
 const expenseName = document.getElementById("product-title");
 const expenseAmount = document.getElementById("user-amount");
 const  checkAmount= document.getElementById("check-amount");
@@ -17,20 +15,20 @@ const  checkAmount= document.getElementById("check-amount");
 
 submit.addEventListener("click", () =>{
 
-    let totAmnt = totalAmount.value;
+    let totAmnt = parseFloat( totalAmount.value);
     if (totAmnt.length == 0 || totAmnt  < 0) 
     {
         alert("Field cannot contain null and negative values");
     } 
-    let tempAmount = 0;
-    tempAmount=totAmnt;
-    incomeAmount.innerHTML = tempAmount;
-    totalAmount.value = '';
 
-    parseInt(expenditureValue.innerText) 
-    balanceAmount.innerHTML = tempAmount - expenditureValue.innerText;
-
-    
+    let totexp = parseFloat( totAmnt);
+    let sumExp = parseFloat(incomeAmount.innerText) + totexp
+    incomeAmount.innerHTML = sumExp;
+     totalAmount.value = '';
+     
+    balanceAmount.innerHTML = parseFloat(incomeAmount.innerText)  - parseFloat(expenditureValue.innerText) ;
+   
+  
 });
  
     checkAmount.addEventListener("click", (e) =>{
@@ -50,12 +48,8 @@ submit.addEventListener("click", () =>{
     let expenditure = parseFloat(expenseAmount.value);
     let sum = parseFloat( expenditureValue.innerText) + expenditure;
     expenditureValue.innerText = sum;
-    
-    let price = 0;
-    price=expenditure; 
-    expenditureValue.innerHTML = price;
     expenseAmount.value = '';
-  
+    balanceAmount.innerHTML = parseFloat( balanceAmount.innerText) - expenditureValue.innerText;
 
      e.preventDefault;
     let list = document.createElement('p')
